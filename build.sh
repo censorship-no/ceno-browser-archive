ec43da1c8c1e68ea17a7c206a005ce5bcdf38988
Peter Jankuliak <p.jankuliak@gmail.com>
2018-08-13 13:48:20 +0200

    Add build.sh

---
+++ build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -e
+
+DIR=`pwd`
+SELF=$0
+ROOT=`dirname $0`
+
+function usage {
+    echo "Usage: $SELF <PATH-TO-OUIFENNEC-ROOT>"
+}
+
+if [ ! -d "$ROOT" ]; then
+    usage
+    [ "$ROOT" == "help" ] && return 0 || return 1
+fi
+
+(cd $ROOT; git submodule update --init --recursive)
+
+function build_ouinet {
+    mkdir -p $DIR/build.ouinet
+    cd $DIR/build.ouinet
+    $ROOT/ouinet/scripts/build-android.sh
+    cd -
+}
+
+function build_oui_fennec {
+    mkdir -p $DIR/build.fennec
+    yes | $ROOT/ouinet/scripts/build-firefox-for-android.sh $ROOT/gecko-dev
+    cd -
+}
+
+#build_ouinet
+build_oui_fennec
