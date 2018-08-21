fdd2f04676be41808811bc9bfe81f454b1ad639d
Peter Jankuliak <p.jankuliak@gmail.com>
2018-08-21 15:18:31 +0200

    HACK to modify the path to ouinet.aar from build.sh

--- build.sh
+++ build.sh
@@ -23,6 +23,21 @@ function build_ouinet {
 function build_oui_fennec {
     mkdir -p $DIR/build.fennec
     cd $DIR/build.fennec
+
+    # HACK: This version of Fennec is patched to use the Ouinet library
+    # and as such the Fennec build process needs to know where to find
+    # that library. Unfortunatelly it's not as easy as passing an argument
+    # to gradle (the android build system) because gradle is being executed
+    # through the Mozilla's 'mach' script.
+    #
+    # Thus as a quick hack, we modify the ouinetArchiveDir variable inside
+    # the build.gradle build script from here to point to the directory
+    # where ouinet-{debug,release}.aar is located.
+
+    local aar_path="$DIR/build.ouinet/build-android/builddir/ouinet/build-android/outputs/aar"
+    local gradle_file=$ROOT/gecko-dev/mobile/android/app/build.gradle
+    sed -i "s|\(\s*def\s\+ouinetArchiveDir\s*=\s*\).*|\1\"${aar_path}\"|" $gradle_file
+
     $ROOT/scripts/build-fennec.sh -m $ROOT/gecko-dev -g $MOZ_GIT
     cd -
 }
