8d90e79c4081f59f4ecca391989bdcadb03c21b6
Sarah Fortune <sarah.fortune@gmail.com>
2019-04-23 10:58:02 -0700

    Update path to release APK in the build script to point to obj-...-release directory

--- scripts/build-fennec.sh
+++ scripts/build-fennec.sh
@@ -168,7 +168,7 @@ if [ $IS_RELEASE_BUILD -eq 1 ]; then
   ./mach gradle app:assembleWithGeckoBinariesRelease
   echo
   echo "Signed release APK:"
-  ls -alh $(realpath obj-arm-linux-androideabi/gradle/build/mobile/android/app/outputs/apk/withGeckoBinaries/release/app-withGeckoBinaries-release.apk)
+  ls -alh $(realpath obj-arm-linux-androideabi-release/gradle/build/mobile/android/app/outputs/apk/withGeckoBinaries/release/app-withGeckoBinaries-release.apk)
 else
   echo 'Result APKs:'
   find $(realpath obj-arm-linux-androideabi/dist) -maxdepth 1 -name '*multi*arm.apk'
